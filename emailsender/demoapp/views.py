f099df7d57fd66ae566b46a979b0cc1a1486ce3e
Chillpill9797 <adityagoswami193@gmail.com>
2024-05-31 18:42:44 +0530

    complete update

--- emailsender/demoapp/views.py
+++ emailsender/demoapp/views.py
@@ -1,11 +1,31 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
 
 # Create your views here.
 def sendanemail(request):
-    return render(
-        request,
-        'email.html',
-        {
-            'title':'send an email'
-        }
-    )
+    if request.method == "POST":
+        to = request.POST.get('toemail')
+        content = request.POST.get('content')
+        # print(to,content)
+        send_mail(
+            "Python (Selenium) Assignment- Aditya Goswami",
+            content,
+            settings.EMAIL_HOST_USER,
+            [to]
+        )
+        return render(
+            request,
+            'email.html',
+            {
+                'title':'send an email'
+            }
+        )
+    else:
+        return render(
+            request,
+            'email.html',
+            {
+                'title':'send an email'
+            }
+        )
--- emailsender/emailsender/settings.py
+++ emailsender/emailsender/settings.py
@@ -123,8 +123,12 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+from dotenv import load_dotenv
+import os
+load_dotenv()
 EMAIL_HOST="smtp.gmail.com"
 EMAIL_PORT=587
-EMAIL_HOST_USER = 'adityagoswami9797@gmail.com'
-EMAIL_HOST_PASSWORD = '85%nitof@!l'
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
